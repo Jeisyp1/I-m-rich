18786ce5d89d571e57af353f0c36694fd5e7113c
Jeisyp1 <jeipanameno@gmail.com>
2025-02-08 20:26:19 -0500

    starting

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// the main function is the starting point for my flutter app
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('Hello Salem'),
+          backgroundColor: Colors.blue),
+        ),
+      ),
+    );
